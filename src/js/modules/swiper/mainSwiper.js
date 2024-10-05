b698e4a921553fd04b0ae56b0acbb94198ecb6b4
Yurchuk Vasyl <mira07081984@gmail.com>
2024-10-05 08:46:44 +0300

    feat: :sparkles: add markup list-services

--- src/js/modules/swiper/mainSwiper.js
+++ src/js/modules/swiper/mainSwiper.js
@@ -6,30 +6,30 @@ const swiperMainPagination = '.swiper-main__pagination';
 let swiperMainLabel;
 
 // config swiper main
-// const swiperMainLabelFun = () => {
-//   const screenWidth = window.innerWidth;
+const swiperMainLabelFun = () => {
+  const screenWidth = window.innerWidth;
 
-//   if (screenWidth > 1024 && !swiperMainLabel) {
-//     swiperMainLabel = new Swiper(swiperMain, {
-//       direction: 'vertical',
-//       slidesPerView: 1,
-//       spaceBetween: 0,
-//       mousewheel: true,
-//       speed: 1000,
-//       pagination: {
-//         el: swiperMainPagination, // Pagination container
-//         clickable: true, // Make bullets clickable
-//         bulletClass: 'swiper-bullet', // Custom bullet class
-//         bulletActiveClass: 'swiper-bullet-active' // Custom active class
-//       }
-//     });
-//   }
+  if (screenWidth > 1024 && !swiperMainLabel) {
+    swiperMainLabel = new Swiper(swiperMain, {
+      direction: 'vertical',
+      slidesPerView: 1,
+      spaceBetween: 0,
+      mousewheel: true,
+      speed: 1000,
+      pagination: {
+        el: swiperMainPagination, // Pagination container
+        clickable: true, // Make bullets clickable
+        bulletClass: 'swiper-bullet', // Custom bullet class
+        bulletActiveClass: 'swiper-bullet-active' // Custom active class
+      }
+    });
+  }
 
-//   if (screenWidth <= 1024 && swiperMainLabel) {
-//     swiperMainLabel.destroy(true, true);
-//     swiperMainLabel = undefined;
-//   }
-// };
+  if (screenWidth <= 1024 && swiperMainLabel) {
+    swiperMainLabel.destroy(true, true);
+    swiperMainLabel = undefined;
+  }
+};
 
 export const initSwiperMainLabel = () => {
   if (document.querySelector(swiperMain)) {
